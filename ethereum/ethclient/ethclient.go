7edf9e97516a9d927bfcf4b3d271f1f18d321eb6
maiiz <maiiz@qq.com>
2018-02-11 16:33:29 +0800

    fix bugs

--- ethereum/ethclient/ethclient.go
+++ ethereum/ethclient/ethclient.go
@@ -180,18 +180,21 @@ func (tx *rpcTransaction) UnmarshalJSON(msg []byte) error {
 }
 
 // TransactionByHash returns the transaction with the given hash.
-func (ec *Client) TransactionByHash(ctx context.Context, hash common.Hash) (tx *types.Transaction, blockNumber *string, err error) {
+func (ec *Client) TransactionByHash(ctx context.Context, hash common.Hash) (tx *types.Transaction, blockNumber string, err error) {
 	var json *rpcTransaction
 	err = ec.c.CallContext(ctx, &json, "eth_getTransactionByHash", hash)
 	if err != nil {
-		return nil, false, err
+		return nil, "", err
 	} else if json == nil {
-		return nil, false, ethereum.NotFound
+		return nil, "", ethereum.NotFound
 	} /*else if _, r, _ := json.tx.RawSignatureValues(); r == nil {
 		return nil, false, fmt.Errorf("server returned transaction without signature")
 	}*/
 	setSenderFromServer(json.tx, json.From, json.BlockHash)
-	return json.tx, json.BlockNumber, nil
+	if json.BlockNumber != nil {
+		return json.tx, *json.BlockNumber, nil
+	}
+	return json.tx, "0", nil
 }
 
 // TransactionSender returns the sender address of the given transaction. The transaction
--- utils/coin.go
+++ utils/coin.go
@@ -1,16 +1,20 @@
 package utils
 
+import (
+	"math/big"
+)
+
 func round(f float64) float64 {
 	return float64(f*100000000) / 100000000
 }
 
 // CoinToFloat converts coin to float64.
-func CoinToFloat(val *big.Int) float64 {
+func CoinToFloat(val, c *big.Int) float64 {
 	bigval := new(big.Float)
 	bigval.SetInt(val)
 
 	coin := new(big.Float)
-	coin.SetInt(params.Params.Coin)
+	coin.SetInt(c)
 	bigval.Quo(bigval, coin)
 
 	fCoin, _ := bigval.Float64()
@@ -18,13 +22,14 @@ func CoinToFloat(val *big.Int) float64 {
 }
 
 // FloatToCoin coverts float64 to coin.
-func FloatToCoin(val float64) *big.Int {
+func FloatToCoin(val float64, c *big.Int) *big.Int {
 	bigval := new(big.Float)
 	bigval.SetFloat64(val)
-	bigval.SetPrec(64)
 
 	coin := new(big.Float)
-	coin.SetInt(params.Params.Coin)
+	coin.SetInt(c)
+	coin.SetPrec(64)
+
 	bigval.Mul(bigval, coin)
 
 	result := new(big.Int)
